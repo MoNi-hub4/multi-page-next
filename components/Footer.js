a67dcbae673ea7acc9863f92e15dbf1381eff2e8
MoNi-hub4 <moniitvf@gmail.com>
2023-06-12 22:23:29 +0530

    sss

--- components/Footer.js
+++ components/Footer.js
@@ -20,9 +20,29 @@ const Footer = () => {
             <ButtonWhite text={"Get in touch"} />
           </div>
         </div>
-        <div className={styles.aboutSection}>
-          <div class={styles.logo}>
-            <Image src={Logo} width={200} height={25} />
+        <div className={styles.footer}>
+          <>
+            <div className={styles.logo}>
+              <Image src={Logo} width={200} height={25} />
+            </div>
+            <div className={styles.navmenu}>
+              <a>Our Company</a>
+              <a>Location</a>
+              <a>Contact</a>
+            </div>
+          </>
+          <div className={styles.section02}>
+            <div className={styles.address1}>
+              <p>Designo Central Office</p>
+              <span>3886 Wellington Street</span>
+              <span>Toronto, Ontario M9C 3J5</span>
+            </div>
+            <div className={styles.address2}>
+              <p>Contact Us (Central Office)</p>
+              <span>P : +1 253-863-8967</span>
+              <span>M : contact@designo.co</span>
+            </div>
+            <div></div>
           </div>
         </div>
       </div>
