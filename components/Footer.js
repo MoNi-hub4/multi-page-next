39bacfcf17adc4e0511fb3ebc835ee68a3368af0
MoNi-hub4 <moniitvf@gmail.com>
2023-06-18 13:31:11 +0530

    HPC

--- components/Footer.js
+++ components/Footer.js
@@ -3,25 +3,34 @@ import { ButtonWhite } from "./Button";
 import styles from "/styles/Footer.module.css";
 import Logo from "/assets/shared/desktop/logo-light.png";
 import Image from "next/image";
+import Facebook from "/assets/shared/desktop/icon-facebook.svg";
+import Instagram from "/assets/shared/desktop/icon-instagram.svg";
+import Pinterest from "/assets/shared/desktop/icon-pinterest.svg";
+import Youtube from "/assets/shared/desktop/icon-youtube.svg";
+import Twitter from "/assets/shared/desktop/icon-twitter.svg";
 
 const Footer = () => {
   return (
     <>
       <div className={styles.section}>
         <div className={styles.footerCard}>
-          <p>
-            <span>Let’s talk about</span> <span>your project</span>
-          </p>
-          <span>
-            Ready to take it to the next level? Contact us today and find out
-            how our expertise can help your business grow.
-          </span>
+          <div className={styles.textsection}>
+            <p className={styles.text}>
+              <span>Let’s talk about</span> <span>your project</span>
+            </p>
+            <span className={styles.subtext}>
+              <p>
+                Ready to take it to the next level? Contact us today and find
+                out how our expertise can help your business grow.
+              </p>
+            </span>
+          </div>
           <div className={styles.button}>
             <ButtonWhite text={"Get in touch"} />
           </div>
         </div>
         <div className={styles.footer}>
-          <>
+          <div className={styles.section01}>
             <div className={styles.logo}>
               <Image src={Logo} width={200} height={25} />
             </div>
@@ -30,7 +39,7 @@ const Footer = () => {
               <a>Location</a>
               <a>Contact</a>
             </div>
-          </>
+          </div>
           <div className={styles.section02}>
             <div className={styles.address1}>
               <p>Designo Central Office</p>
@@ -42,7 +51,13 @@ const Footer = () => {
               <span>P : +1 253-863-8967</span>
               <span>M : contact@designo.co</span>
             </div>
-            <div></div>
+            <div className={styles.icons}>
+              <Image src={Facebook}></Image>
+              <Image src={Youtube}></Image>
+              <Image src={Twitter}></Image>
+              <Image src={Pinterest}></Image>
+              <Image src={Instagram}></Image>
+            </div>
           </div>
         </div>
       </div>
