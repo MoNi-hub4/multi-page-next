8e24680ed971fc91c23c3e8bc15e6af26d5ecb5e
MoNi-hub4 <moniitvf@gmail.com>
2023-06-21 12:58:24 +0530

    sss

--- app/webdesign/page.js
+++ app/webdesign/page.js
@@ -1,6 +1,11 @@
 import styles from "/styles/webdesign.module.css";
 import Image from "next/image";
 import Webdesign1 from "/assets/web-design/desktop/image-blogr.jpg";
+import Webdesign2 from "/assets/web-design/desktop/image-builder.jpg";
+import Webdesign3 from "/assets/web-design/desktop/image-camp.jpg";
+import Webdesign4 from "/assets/web-design/desktop/image-express.jpg";
+import Webdesign5 from "/assets/web-design/desktop/image-photon.jpg";
+import Webdesign6 from "/assets/web-design/desktop/image-transfer.jpg";
 
 function Webdesign() {
   return (
@@ -16,26 +21,58 @@ function Webdesign() {
       </div>
       <div className={styles.imageSection}>
         <div className={styles.imageContainer}>
-          <Image src={Webdesign1} ></Image>
+          <Image src={Webdesign4}></Image>
           <div className={styles.description}>
             <p>Express</p>
-            <span>A multi-carrier shipping website for ecommerce businesses</span>
+            <span>
+              A multi-carrier shipping website for ecommerce businesses
+            </span>
           </div>
         </div>
         <div className={styles.imageContainer}>
-          <Image src={Webdesign1} ></Image>
+          <Image src={Webdesign6}></Image>
+          <div className={styles.description}>
+            <p>Express</p>
+            <span>
+              A multi-carrier shipping website for ecommerce businesses
+            </span>
+          </div>
         </div>
         <div className={styles.imageContainer}>
-          <Image src={Webdesign1} ></Image>
+          <Image src={Webdesign5}></Image>
+          <div className={styles.description}>
+            <p>Express</p>
+            <span>
+              A multi-carrier shipping website for ecommerce businesses
+            </span>
+          </div>
         </div>
         <div className={styles.imageContainer}>
-          <Image src={Webdesign1} ></Image>
+          <Image src={Webdesign2}></Image>
+          <div className={styles.description}>
+            <p>Express</p>
+            <span>
+              A multi-carrier shipping website for ecommerce businesses
+            </span>
+          </div>
         </div>
         <div className={styles.imageContainer}>
-          <Image src={Webdesign1} ></Image>
+          <Image src={Webdesign1}></Image>
+          <div className={styles.description}>
+            <p>Express</p>
+            <span>
+              A multi-carrier shipping website for ecommerce businesses
+            </span>
+          </div>
         </div>
         <div className={styles.imageContainer}>
-          <Image src={Webdesign1} ></Image>
+          <Image src={Webdesign3}></Image>
+          <div className={styles.description}>
+            <p>Express</p>
+            <span>
+              A multi-carrier shipping website for ecommerce businesses
+            </span>
+          </div>
         </div>
       </div>
     </>
