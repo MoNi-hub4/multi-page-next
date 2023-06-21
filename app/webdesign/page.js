a4cc06cee61f873e984284864ee0c42932c31a89
MoNi-hub4 <moniitvf@gmail.com>
2023-06-21 12:05:28 +0530

    ss

--- app/webdesign/page.js
+++ app/webdesign/page.js
@@ -1,5 +1,45 @@
-function Webdesign(){
-    return <h1>Webdesign</h1>
+import styles from "/styles/webdesign.module.css";
+import Image from "next/image";
+import Webdesign1 from "/assets/web-design/desktop/image-blogr.jpg";
+
+function Webdesign() {
+  return (
+    <>
+      <div className={styles.section}>
+        <p className={styles.title}>Web Design</p>
+        <span>
+          <p className={styles.subtitle}>
+            We build websites that serve as powerful marketing tools and bring
+            memorable brand experiences.
+          </p>
+        </span>
+      </div>
+      <div className={styles.imageSection}>
+        <div className={styles.imageContainer}>
+          <Image src={Webdesign1} ></Image>
+          <div className={styles.description}>
+            <p>Express</p>
+            <span>A multi-carrier shipping website for ecommerce businesses</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src={Webdesign1} ></Image>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src={Webdesign1} ></Image>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src={Webdesign1} ></Image>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src={Webdesign1} ></Image>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src={Webdesign1} ></Image>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Webdesign
+export default Webdesign;
