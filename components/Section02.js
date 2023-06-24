382a39088f40313c9c7cc7f8e82998502ec133ae
MoNi-hub4 <moniitvf@gmail.com>
2023-06-24 15:01:37 +0530

    web fixed

--- components/Section02.js
+++ components/Section02.js
@@ -6,20 +6,20 @@ import Link from "next/link";
 const Section02 = (props) => {
   return (
     <>
-      <div class={styles.section}>
-        {!props.isWeb && <div class={`${styles.webdesign} ${styles.gridchild}`}>
+      <div class={`${styles.section} ${props.isWeb || props.isApp || props.isDesign ? styles.newClass : ''}`}>
+        {!props.isWeb && <div class={`${styles.webdesign} ${styles.gridchild} ${props.isApp || props.isDesign ? styles.newClass : ''}`}>
           Web Design
           <Link href="/webdesign">
             <span>View Projects</span>
           </Link>
         </div>}
-        {!props.isApp && <div class={`${styles.appdesign} ${styles.gridchild}`}>
+        {!props.isApp && <div class={`${styles.appdesign} ${styles.gridchild} ${props.isWeb ? styles.newClass : ''}`}>
           App Design
           <Link href="/appdesign">
             <span>View Projects</span>
           </Link>
         </div>}
-        {!props.isDesign && <div class={`${styles.graphicdesign} ${styles.gridchild}`}>
+        {!props.isDesign && <div class={`${styles.graphicdesign} ${styles.gridchild} ${props.isWeb ? styles.newClass : ''}`}>
           Graphic Design
           <Link href="/graphicdesign">
             <span>View Projects</span>
